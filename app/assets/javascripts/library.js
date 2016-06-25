1d987e46b4420044cb507a4eabacb51d6da1c9d9
mradzwilla <mradzwilla@gmail.com>
2016-06-25 03:40:08 -0400

    Began working on battle functions and screen layout.

--- app/assets/javascripts/library.js
+++ app/assets/javascripts/library.js
@@ -6,9 +6,12 @@ function preload(){
 	game.load.spritesheet('aladdin', 'assets/aladdin.png', 32, 48, 16)
 	game.load.spritesheet('belle', 'assets/belle.png', 32, 48, 16)
 	game.load.spritesheet('textBox', 'assets/textBoxes.png')
+	game.load.spritesheet('battleScreen', 'assets/battleBackgrounds.png')
+
 
 	game.state.add('levelOne', levelOne)
 	game.state.add('menu', gameMenu)
+	game.state.add('battle', battleScreen)
 	console.log("preload")
 
 	dialogueFlag = 0
@@ -18,6 +21,35 @@ function create(){
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 	console.log('create')
 	game.state.start('menu')
+
+}
+
+function battle(){
+	game.state.start('battle')
+
+}
+
+var battleScreen = {
+
+	create: function(){
+		var image = game.add.sprite(0,0, 'battleScreen')
+		var enemyImage = game.add.sprite(550, 200, villain.sprite, 0)
+		enemyImage.scale.set(3,3)
+		var enemyName = game.add.text(150, 110, villain.name)
+		var enemyHealth = game.add.text(150, 140, 'Health:' + villain.health)
+		var playerImage = game.add.sprite(100, 450, 'aladdin', 12)
+		playerImage.scale.set(5,5)
+
+		var playerSkills = game.add.group();
+		playerSkills = {'Fight':'Kick', 'Item':'Lamp'}
+
+		for (i=0;i<=1;i++){
+			game.add.text(500, 450 + (i * 30), Object.keys(playerSkills)[i])
+		}
+
+		var cursor = playerSkills.cursor
+	}
+
 
 }
 
@@ -77,18 +109,19 @@ var levelOne = {
 			}
 		}
 
-		function dialogue(text) {
+		function dialogue(encounter) {
 			
 			//Stop velocity to prevent player movement during dialogue
 			player.body.velocity.x = 0
 			player.body.velocity.y = 0
 
+			// battle("belle");
+
 			dialogueFlag = 1			
-			var speech = text
+			var speech = encounter.text
 			var i = -1 	//The downDuration functionwill immediately fire setting i to 0
 			textBox = game.add.sprite(0, 500, 'textBox')
 
-			console.log("initialize dialogue")
 			textDisplay = game.add.text(50, 532, speech[0],{fill: "#444444"});
 
 			levelOne.state.update = function(){
@@ -100,11 +133,17 @@ var levelOne = {
 
 				//-1 ommitted from speech.length lets pressing space close the box
 				if (i == (speech.length)){
+
+					if (encounter.battle == 1){
+						villain = encounter.enemy
+						battle("we fightin now")
+					} else {
 					textDisplay.destroy();
 					textBox.destroy()
 					dialogueFlag = 0;
 					movementControls();
 					return
+					}
 				}
 			}
 		}
@@ -116,6 +155,8 @@ var levelOne = {
 		belle.body.immovable = true
 		belle.body.physicsBodyType = Phaser.Physics.ARCADE;
 		belle.body.setSize(24,36)
+
+		var enemyBelle = {'name': "Belle", 'sprite': 'belle', 'health' : 25}
 
 		var player = game.add.sprite(32, game.world.height - 150, 'aladdin');
 		player.enableBody = true
@@ -142,7 +183,7 @@ var levelOne = {
 
 			pressSpace.onDown.add(function(){
 				if (game.physics.arcade.distanceBetween(player,belle) <= 45 && dialogueFlag==0){
-					dialogue(["Oh hello...street rat.","Why The Purge is a tale as old as time!","It's purging time!"]);
+					dialogue({text: ["Oh hello...street rat.","Why The Purge is a tale as old as time!","It's purging time!"], battle: 1, enemy: enemyBelle});
 					}
 				})
 			}
